ce4cf4378e7ec9dd41e4eb60d5b8d5c7e714c4d5
Mark Dz <markdz@MacBook-Pro-Mark.local>
2016-09-11 15:12:20 +0200

    Add Toolbar and Menu Options

--- app/src/main/java/com/dziewit/marek/schultztablesgame/MainActivity.java
+++ app/src/main/java/com/dziewit/marek/schultztablesgame/MainActivity.java
@@ -1,13 +1,52 @@
 package com.dziewit.marek.schultztablesgame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.tool_bar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(
+                    this, drawerLayout, toolbar,
+                    R.string.drawer_title, R.string.drawer_title
+            );
+            drawerLayout.addDrawerListener(mDrawerToggle);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            mDrawerToggle.syncState();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.clear();
+        for (int i = 0; i < 6; i++) {
+            menu.add(String.format("%sx%s", i + 2, i + 2));
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 }
