a0d2fe914cf81a24cc4ab852d6b7adf0f64e5536
Mark Dz <markdz@MacBook-Pro-Mark.local>
2016-09-12 01:48:28 +0200

    Main Activity UT Menu Items

--- app/src/test/java/com/dziewit/marek/schultztablesgame/MainActivityTest.java
+++ app/src/test/java/com/dziewit/marek/schultztablesgame/MainActivityTest.java
@@ -1,5 +1,9 @@
 package com.dziewit.marek.schultztablesgame;
 
+import android.view.Menu;
+
+import com.dziewit.marek.schultztablesgame.custom.views.ShultzTableView;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +14,13 @@ import org.robolectric.annotation.Config;
 import org.robolectric.util.ActivityController;
 import org.robolectric.util.ReflectionHelpers;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.robolectric.Shadows.shadowOf;
 
 /**
  * Created by Marek Dziewit
@@ -22,6 +32,8 @@ public class MainActivityTest {
 
     public static final String TOOLBAR_FIELD_NAME = "toolbar";
     public static final String DRAWER_LAYOUT_FIELD_NAME = "drawerLayout";
+    public static final int MENU_ITEMS_COUNT = 6;
+    public static final String TABLE_VIEW_FIELD_NAME = "tableView";
 
     private ActivityController<MainActivity> mainActivityActivityController;
 
@@ -42,4 +54,28 @@ public class MainActivityTest {
         assertNotNull(ReflectionHelpers.getField(mainActivityActivityController.get(), DRAWER_LAYOUT_FIELD_NAME));
     }
 
+    @Test
+    public void test_MenuCreation() throws Exception {
+        MainActivity mainActivity = mainActivityActivityController.create().visible().get();
+
+        Menu menu = shadowOf(mainActivity).getOptionsMenu();
+
+        assertEquals(MENU_ITEMS_COUNT, menu.size());
+
+        for (int i = 0; i < MENU_ITEMS_COUNT; i++) {
+            assertEquals(String.format("%sx%s", i + 2, i + 2), menu.getItem(i).getTitle());
+        }
+    }
+
+    @Test
+    public void test_OnOptionItemSelected() throws Exception {
+        MainActivity mainActivity = mainActivityActivityController.create().visible().get();
+        ShultzTableView tableView = mock(ShultzTableView.class);
+
+        ReflectionHelpers.setField(mainActivity, TABLE_VIEW_FIELD_NAME, tableView);
+
+        shadowOf(mainActivity).clickMenuItem(0);
+
+        verify(tableView, times(1)).refreshView(eq(2), eq(2));
+    }
 }
